9825e0af19e4df14f97176d173608ef0d32e584d
edsonMark <mark.ofili@sainsburys.co.uk>
2016-11-18 13:37:05 +0000

    Update RegisterSpec.js

--- RegisterSpec.js
+++ RegisterSpec.js
@@ -1,48 +1,48 @@
   /**
-   * Protractor e2e test spec
-   * Started e2e file test
-   */
 
+   */
   //Task//
 
   //Variables
-  const Register = require('../Register/register_pageObject.js');
+  const register = require('../Register/register_pageObject.js');
   //
 
   beforeEach (() => {
     browser.ignoreSynchronization = true;
-   });
+  });
 
   beforeEach (() => {
     browser.manage().deleteAllCookies();
     browser.get('https://www.marksandspencer.com/');
-    browser.driver.manage().window().maximize();
     browser.driver.manage().timeouts().implicitlyWait(1500);
   });
 
   function DisplayForm() {
-    Register.lnkRegister.click();
-  }
-
-  function DisplayForm() {
-    Register.lnkRegister.click();
+    register.lnkRegister.click();
   }
 
   describe('Register form', () => {
 
     it('Should be displayed when the "Register" link is clicked', () => {
+
       DisplayForm();
-      expect(Register.txtFirstname.isDisplayed()).toBe(true);
-      expect(Register.txtLastname.isDisplayed()).toBe(true);
+      expect(register.txtFirstname.isDisplayed()).toBe(true);
     });
 
-    it('Should validate mandatory and or empty fields', () => {
+    it('Should validate mandatory field', () => {
 
       var EC = protractor.ExpectedConditions;
       DisplayForm();
-      Register.fillForgetForm(' ', ' ', ' ', ' ', ' ');
-      browser.wait(EC.textToBePresentInElement($('Register.txtFirstname'), 'This field is required.'), 5000);
-  ;//firstname
+      register.fillForgetForm(' ', ' ', ' ', ' ', ' ');
+      register.btnCreateAccount();
+      browser.wait(EC.textToBePresentInElement($('register.txtFirstname'), 'This field is required.'), 5000);//firstname
+    });
+
+    it('Should not allow invalid email format', () => {
+
+      DisplayForm();
+      register.fillForgetForm('', '', 'invalid', '', '');
+      expect(register.lblErrorMessage.isDisplayed()).toBe(true);
     });
 
   });
