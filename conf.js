5bc2199fe6b36f6061fd3b4de1f633005e8b7211
Mark <mark.ofili@sainsburys.com>
2016-11-22 22:31:48 +0000

    test

--- conf.js
+++ conf.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 // conf.js
+
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./Register/RegisterSpec.js']
 }
-=======
-// conf.js
-exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['./Register/RegisterSpec.js']
-}
->>>>>>> 61f8fae66dd740760cbd5db9621eb59353e9365d
